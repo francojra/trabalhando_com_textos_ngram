b09e3771a7312855a5defcb8801dbd9c59604dd1
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-05-03 23:10:56 -0300

    Add files via upload

--- trabalhando_textos_ngram.R
+++ trabalhando_textos_ngram.R
@@ -41,3 +41,101 @@ view(casmurro_3)
 freq_3 <- casmurro_3 %>%
   count(palavra, sort = TRUE)
 view(freq_3)
+
+# Exercícios -------------------------------------------------------------------------------------------------------------------------------
+
+### Baixe um texto de tamanho relativamente grande. Recomendável usar o pacote 'gutenbergr'
+### ou 'rvest'.
+
+library(rvest)
+
+### "Serenata" é um projeto aberto no qual usa data science (ciência de dados) – a mesma 
+### tecnolgia usada por gigantes como Google, Facebook e Netflix – com o objetivo de 
+### monitorar os gastos públicos e compartilhar informações de forma acessível a todos.
+
+### Explicado o projeto Serenata, voltamos ao Web Scraping. A coleta de dados será sobre 
+### o 'Jarbas', que é a plataforma onde ficam armazenados os documentos considerados 
+### suspeitos.
+
+url <- "https://jarbas.serenata.ai/dashboard/chamber_of_deputies/reimbursement/"
+jarbas_webpage <- read_html(url)
+
+### Passo 2: Agora, a melhor parte da biblioteca rvest é que você pode extrair os dados 
+### de html em forma de nós, o que significa que você pode selecionar imediatamente 
+### quais os nós através dos ids ou classes css e extrair o texto das tags do html. 
+### Então eu fui para o meu url e abri o “firebug ” no navegador e percebi que os nomes 
+### dos deputados foram encapsulados na classe css “.field-congressperson_name ”, usando 
+### esta classe css que posso extrair todos os nomes dos deputados na página da web.
+
+### Existem 2 funções que usaremos aqui:
+
+### html_nodes: Use esta função para extrair os nós que desejamos (neste caso nós com 
+### “.field-congressperson_name” como classe css
+### html_text: Use esta função para extrair o texto entre dos nós html (neste caso, 
+### os nomes de nossos representantes)
+
+#Scraping  usando classe css ‘field-congressperson_name’
+jarbas_names_html <- html_nodes(jarbas_webpage, '.field-congressperson_name')
+jarbas_names <- html_text(jarbas_names_html)
+head(jarbas_names)
+
+### Da mesma forma, agora farei isso para todos os outros atributos: SUBQUOTA TRANSLATED, 
+### FORNECEDOR. Cada um desses atributos tem suas próprias classes css: 
+### field-subquota_translated, field-supplier_info.
+
+#SUBQUOTA TRANSLATED
+jarbas_subquota_html <- html_nodes(jarbas_webpage, '.field-subquota_translated')
+jarbas_subquota <- html_text(jarbas_subquota_html)
+head(jarbas_subquota)
+
+#Fornecedor
+jarbas_provider_html <- html_nodes(jarbas_webpage, '.field-supplier_info')
+jarbas_provider <- html_text(jarbas_provider_html)
+head(jarbas_provider)
+
+### A seguir serão mostrados o valor do reembolso, note que eles são extraídos em tipo
+### de variável caracter: Ex: R$ 139,76, R$ 40,23. Entretanto, como desejamos manipular 
+### esses números, precisamos convertê-los para tipo de variável numérica, dessa forma 
+### será utilizado a biblioteca: “Stringr” mais especificamente a função: str_extract. 
+### Segue o script para conversão da variável.
+
+library(stringr)
+#valores em Real
+jarbas_value_html <- html_nodes(jarbas_webpage, '.field-value')
+jarbas_value <- html_text(jarbas_value_html)
+head(jarbas_value)
+
+#dados extraídos na forma de caracter, vamos converter para tipo numérico
+jarbas_value <- as.numeric(sub(",",".", str_extract(jarbas_value, 
+                                                   pattern = "\\d+")))
+head(jarbas_value, 50)
+
+### Passo 3: Com essas informações disponíveis, vamos gerar um dataframe. Para facilitar 
+### a visualização, apenas o primeiro nome de cada deputado foi selecionado.
+
+#Combinando todas as características obtidas
+jarbas_names <- str_extract(jarbas_names, pattern = boundary("word"))
+jarbas_df <- data.frame(
+  Name = jarbas_names,
+  Subquota = jarbas_subquota,
+  Provider = jarbas_provider,
+  Value = jarbas_value
+  )
+str(jarbas_df)
+view(jarbas_df)
+
+### Passo 4: Numa primeira análise, podemos utilizar gráfico entre o valor do reembolso 
+### e o nome dos deputados. Foram utilizados as 80 primeiras linhas do data.frame e uma 
+### biblioteca ggplot2 para geração do gráfico.
+
+library(ggplot2)
+
+jarbas_df <- jarbas_df[1:80,]
+
+ggplot(
+  jarbas_df, aes(Value, Name, colour = Subquota)) +
+  geom_point() +
+  labs(title = "", x = "pedidos de reembolso (R$)",
+       y = "deputados", colour = "SUBQUOTA TRANSLATED")
+
+### Fonte: http://estatidados.com.br/introducao-a-web-scraping-com-r/
